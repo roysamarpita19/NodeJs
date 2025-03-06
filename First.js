5d2f5326feed8c587f2461d2b399a2e34a9d0fa9
roysamarpita19 <itssamarpitaroy19@gmail.com>
2025-03-06 11:18:56 +0530

    adding new files

---
+++ First.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+// Addition
+var a = 200;
+var b = 100;
+var sum = a + b;
+
+
+console.log(sum)
+
+// Substraction
+var sub = a-b;
+console.log(sub)
+
+// Multiplication
+var mul = a*b;
+console.log(mul)
+
+// Division
+var div = a/b;
+console.log(div)
+
+// Area of triangle
+var AreaTri =   1/2*a*b; /* height=a,base=b*/
+console.log(AreaTri)
+
+// Area of circle
+var AreaCir = 2* 3.14*a /*radius=a*/
+console.log(AreaCir)
+
+// Area of cube
+var AreaCube = a*a /*side = a*/
+console.log(AreaCube)
+
+// Perimeter of circle
+var PeriCir = 2*3.14*a /*radius = a*/
+console.log(PeriCir)
+
+// Odd or Even
+
+if (a%2 ==0) {
+    console.log('The number is even')
+  } else {
+    console.log('The number is odd')
+  }
+// Positive or Negative
+
+if (a>0) {
+    console.log('The number is positive')
+  } else {
+    console.log('The number is negative')
+  }
