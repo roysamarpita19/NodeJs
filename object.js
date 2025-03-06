6a1c9769421a98896f50887c0ed5f9df5f1eb0c5
roysamarpita19 <itssamarpitaroy19@gmail.com>
2025-03-06 11:24:56 +0530

    adding new files

---
+++ object.js
@@ -0,0 +1,15 @@
+var student={
+    name:"Shilpa",
+    age:21,
+    roll:180
+}
+
+
+var student2={
+    name:"Samarpita",
+    age:21,
+    roll:160
+}
+
+console.log(student.name, student.age, student.roll)
+console.log(student2.name, student2.age, student2.roll)
