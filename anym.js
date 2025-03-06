7a21b2d45121035884e7867b494acc56dc3635e6
roysamarpita19 <itssamarpitaroy19@gmail.com>
2025-03-06 11:54:24 +0530

    adding new files

---
+++ anym.js
@@ -0,0 +1,36 @@
+var user = function(){
+    var a =10;
+var b =20;
+var r =5;
+var pi =3.14;
+var sum = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+var area = (1/2) * a * b; 
+var cirarea = 2*pi*r;
+var areacube= a*b;
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(area)
+console.log(cirarea)
+console.log(cirarea)
+if (a>0){
+    console.log("The number is positive");
+}
+
+else{
+    console.log("The number is negative");
+}
+if (a%2==0){
+    console.log("The number is even");
+}
+
+else{
+    console.log("The number is odd");
+}
+
+}
+user()
